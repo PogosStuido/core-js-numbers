64a78a42d58abf64432a1cfbc0f38ea57c797d04
Pogos Torosian <pogosstudio@gmail.com>
2025-09-20 19:47:02 +0400

    feat: solve issafeinteger task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -465,8 +465,11 @@ function getIntegerOnString(/* str, base */) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  if (Number.isSafeInteger(number) && number === Math.trunc(number)) {
+    return true;
+  }
+  return false;
 }
 
 /**
