95b4683abfb612a106130e63f76093a1470ff940
Pogos Torosian <pogosstudio@gmail.com>
2025-09-20 22:16:41 +0400

    feat: solve isprime task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -198,8 +198,17 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); ) {
+    if (n % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+  return true;
 }
 
 /**
